5a2ded31f84ec8c7753c56c4cefe7e77a904563f
ethicnology <ethicnology@pm.me>
2023-12-26 09:23:19 -0500

    feat: dotenv

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-A sample command-line application providing basic argument parsing with an entrypoint in `bin/`.
+# Prepare
+
+## dotenv
+
+**.env** has to be in the same folder as the arte executable (.exe/.sh)
+```sh
+cp .env.example .env
+```
+
+## database
+
+**restore dump** schema and basic data
+```sh
+psql -h localhost -U postgres -d postgres < migrations/001_schema.sql
+psql -h localhost -U postgres -d postgres < migrations/002_data.sql
+```
+
+**dump** database `data only`
+```sh
+pg_dump -h localhost -U postgres -n public --data-only > data.sql
+```
+
+# Usage
+```sh
+arte.sh --mode film
+arte.sh --mode film --force
+arte.sh --mode film --arte 083874-000-A
+```
+
--- bin/global.dart
+++ bin/global.dart
@@ -1,15 +1,11 @@
-import 'dart:io';
 import 'dart:math';
 
+import 'package:dotenv/dotenv.dart';
 import 'package:path/path.dart';
 import 'package:supabase/supabase.dart';
 import 'package:logging_colorful/logging_colorful.dart';
 
 final log = LoggerColorful('arte');
-
-final url = Platform.environment['SUPABASE_URL'];
-final key = Platform.environment['SUPABASE_KEY'];
-final supabase = SupabaseClient(url!, key!);
 
 const arteLanguages = <String>["fr", "de", "en", "es", "pl", "it"];
 
@@ -19,6 +15,12 @@ const ips = ['37.187.124.59', '185.246.211.194', '185.246.211.194'];
 
 final random = Random();
 
-final appData = join(Directory.current.path, 'arte_data');
+final _env = DotEnv(includePlatformEnvironment: true)..load();
+final _url = _env['SUPABASE_URL'];
+final _key = _env['SUPABASE_KEY'];
+final _path = _env['OUTPUT_PATH'];
+final supabase = SupabaseClient(_url!, _key!);
+
+final appData = join(_path!, 'arte_data');
 final covers = join(appData, 'covers');
 final subtitles = join(appData, 'subtitles');
