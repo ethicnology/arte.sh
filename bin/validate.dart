8135ba1baba68d6c9bbfb55f338bd3dfb09d01ee
ethicnology <ethicnology@pm.me>
2024-01-07 17:52:22 -0500

    feat: validate film, episode, collection

--- bin/validate.dart
+++ bin/validate.dart
@@ -1,13 +1,41 @@
 import 'global.dart';
 
 class Validate {
-  static bool idFilm(String idArte) {
+  static bool isArteId(String? idArte) {
+    return Validate.isFilm(idArte) ||
+        Validate.isEpisode(idArte) ||
+        Validate.isCollection(idArte);
+  }
+
+  static bool isFilm(String? idArte) {
+    if (idArte == null) return false;
     var parts = idArte.split('-');
-    if (parts.length != 3) throw Exception('invalid id');
-    if (parts[0].length != 6) throw Exception('first part lenght');
-    if (parts[1].length != 3) throw Exception('second part lenght');
-    if (parts[2].length != 1) throw Exception('last part lenght');
-    if (int.parse(parts[1]) != 0) throw Exception('not a movie');
+    if (parts.length != 3) return false;
+    if (parts[0].length != 6) return false;
+    if (parts[1].length != 3) return false;
+    if (parts[2].length != 1) return false;
+    if (int.parse(parts[1]) != 0) return false;
+    return true;
+  }
+
+  static bool isEpisode(String? idArte) {
+    if (idArte == null) return false;
+    var parts = idArte.split('-');
+    if (parts.length != 3) return false; // invalid id
+    if (parts[0].length != 6) return false; // first part lenght
+    if (parts[1].length != 3) return false; // second part lenght
+    if (parts[2].length != 1) return false; // last part lenght
+    if (int.parse(parts[1]) <= 0) return false; // not an episode
+    return true;
+  }
+
+  static bool isCollection(String? idArte) {
+    if (idArte == null) return false;
+    var parts = idArte.split('-');
+    if (parts.length != 2) return false;
+    if (parts[0].length != 2) return false;
+    if (parts[1].length != 6) return false;
+    if (parts[0] != 'RC') return false;
     return true;
   }
 
