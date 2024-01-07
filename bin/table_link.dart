2609f9a0dde15214619c413f5dba43df57ad868a
ethicnology <ethicnology@pm.me>
2024-01-07 17:55:10 -0500

    feat: table link

---
+++ bin/table_link.dart
@@ -0,0 +1,22 @@
+import 'global.dart';
+
+class Link {
+  static const table = 'link';
+
+  int idParent;
+  int idChild;
+  Link({required this.idParent, required this.idChild});
+
+  Future<bool> insert() async {
+    try {
+      var insert = await supabase
+          .from(table)
+          .insert({'id_parent': idParent, 'id_child': idChild}).select();
+      log.fine('$idParent␟$table␟$idChild␟${insert.first['id']}');
+      return true;
+    } catch (e) {
+      log.warning('$idParent␟$table␟$idChild␟${e.toString()}');
+      return false;
+    }
+  }
+}
