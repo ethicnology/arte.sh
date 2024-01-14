380c5dfbb3fd977646ab9553d104b1bb7117b977
ethicnology <ethicnology@pm.me>
2024-01-14 18:48:06 -0500

    refactor: variable name

--- bin/table_provider.dart
+++ bin/table_provider.dart
@@ -4,8 +4,8 @@ class Provider {
   static const table = 'provider';
 
   static Future<int> getId(String label) async {
-    final thing = await supabase.from(table).select().eq('label', label);
-    log.info('$label␟$table␟${thing.first['id']}');
-    return thing.first['id'];
+    final select = await supabase.from(table).select().eq('label', label);
+    log.info('$label␟$table␟${select.first['id']}');
+    return select.first['id'];
   }
 }
